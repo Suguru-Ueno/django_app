38ae22b68eb23813c69357cf436c3392ef8c5e3f
Suguru_Ueno <sgr_ueno@outlook.com>
2020-11-05 18:54:26 +0900

    view関数の処理をTempleteViewクラスで記述することを勉強した。

--- mkfri/views.py
+++ mkfri/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .forms import HelloForm
 
 # Create your views here.
 
-def index(request):
-  params = {
-    'title': 'mkfri/index',
-    'msg' : '名前を入力してください。',
-    'goto' : 'next',
-    'form' : HelloForm(),
-  }
+class HelloView(TemplateView):
 
-  if (request.method == 'POST'):
-    params['msg'] = '名前:' + request.POST['name'] + '<br>メール:' + request.POST['mail'] + '<br>年齢:' + request.POST['age']
-    params['form'] = HelloForm(request.POST)
+  def __init__(self):
+    self.params = {
+      'title' : 'mkfri/index',
+      'msg' : 'あなたの情報を入力してください。',
+      'form' : HelloForm(),
+    }
 
-  return render(request, 'mkfri/index.html', params)
+  def get(self, request):
+    return render(request, 'mkfri/index.html', self.params)
+
+  def post(self, request):
+    self.params['msg'] = '名前:' + request.POST['name'] + '<br>メール:' + request.POST['mail'] + '<br>年齢:' + request.POST['age']
+    self.params['form'] = HelloForm(request.POST)
+    return render(request, 'mkfri/index.html', self.params)
+  
 
 
