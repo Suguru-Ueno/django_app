e919404310e3872ead8e5450910071d8744c1725
Suguru_Ueno <sgr_ueno@outlook.com>
2020-11-21 18:24:19 +0900

    ミニsns作成に必要なモデルを定義し、 admin管理サイトで管理できるようにregisterした。

--- sns/admin.py
+++ sns/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Message, Good, Group, Friend
 # Register your models here.
+admin.site.register(Message)
+admin.site.register(Friend)
+admin.site.register(Group)
+admin.site.register(Good)
