1ef7b5361bbdda243361212debe3bc89650248a0
Suguru_Ueno <sgr_ueno@outlook.com>
2020-11-24 17:25:16 +0900

    Groupの選択メニューフォーム, Friendのチェックボックスフォーム, Group作成フォームの作成

--- sns/forms.py
+++ sns/forms.py
@@ -33,3 +33,23 @@ class GroupCheckForm(forms.Form):
     public = User.objects.filter(username='public').first()
     self.fields['groups'] = forms.MultipleChoiceField(choices=[(item.title, item.title) for item in Group.objects.filter(owner__in=[user, public])], widget=forms.CheckboxSelectMultiple(),)
 
+#Groupの選択メニューフォーム
+class GroupSelectForm(forms.Form):
+  def __init__(self, user, *args, **kwargs):
+    super(GroupSelectForm, self).__init__(*args, **kwargs)
+    self.fields['groups'] = forms.ChoiceField(choices=[('-','-')] + [(item.title, item.title) for item in Group.objects.filter(owner=user)], widget=forms.Select(attrs={'class':'form-control'}),)
+
+#Friendのチェックボックスフォーム
+class FriendsForm(forms.Form):
+  def __init__(self, user, friend=[],vals=[], *args, **kwargs):
+    super(FriendsForm, self).__init__(*args, **kwargs)
+    self.fields['friends'] = forms.MultipleChoiceField(choices=[(item.user, item.user) for item in friends], widget=forms.CheckboxSelectMultiple(), initial=vals)
+
+#Group作成フォーム
+class PostForm(forms.Form):
+  content = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class':'form-control', 'row':2}))
+
+  def __init__(self, user, *args, **kwargs):
+    super(PostForm, self).__init__(*args, **kwargs)
+    public = User.objects.filter(username='public').first()
+    self.fields['group'] = forms.ChoiceField(choices=[('-','-')] + [(item.title, item.title) for item in Group.objects.filter(owner__id=[user,public])], widget=forms.Select(attrs={'class':'form-control'}),)
