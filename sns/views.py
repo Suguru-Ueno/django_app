3a22073fa375fddd1855f0084dcb6b5af6ffd6d7
Suguru_Ueno <sgr_ueno@outlook.com>
2020-11-25 14:51:19 +0900

    ivews.groupの作成

--- sns/views.py
+++ sns/views.py
@@ -53,4 +53,72 @@ def index(request, page=1):
 
 
 @login_required(login_url='/admin/login/')
-def 
+def group(request):
+  #自分が登録したfriendsを取得
+  friends = Friend.objects.filter(owner=request.user)
+
+  #POST時の処理
+  if request.method == 'POST':
+
+    #Groupsメニューの選択肢の処理
+    if request.POST['mode'] == '__groups_form__': #ここは調べる
+      #選択したGroup名を取得
+      sel_group = request.POST['groups']
+      #Groupを取得
+      gp = Group.objects.filter(owner=request.user).filter(title=sel_group).first()
+      
+      #Groupに含まれるFriendを取得
+      fds = Friend.objects.filter(owner=request.user).filter(group=gp)
+      print(Friend.objects.filter(owner=request.user))
+
+      #FriendのUserをリストにまとめる
+      vlist =[]
+      for item in fds:
+        vlist.append(item.user.username)
+
+      #フォームの用意
+      groupsform = GroupSelectForm(request.user, request.POST)
+      friendsform = FriendsForm(request.user, friends=friends, vals=vlist)
+
+    #Friendsのチェック更新時の処理
+    if request.POST['mode'] == '__friends_form__':
+      #選択したGroupの取得
+      sel_group = request.POST['group']
+      group_obj = Group.objects.filter(title=sel_group).first()
+      print(group_obj)
+      #チェックしたFriendsの取得
+      sel_fds = request.POST.getlist('friends')
+      #FriendsのUserの取得
+      sel_user = User.objects.filter(username__in=sel_fds)
+      #Userのリストに含まれるユーザーが登録したFriendを取得
+      fds = Friend.objects.filter(owner=request.user).filter(user__in=sel_user)
+      #すべてのFriendにGroupを設定する
+      vlist = []
+      for item in fds:
+        item.group = group_obj
+        item.save()
+        vlist.append(item.user.username)
+      #メッセージを設定
+      messages.success(request, 'チェックされたFriendを'+sel_group+'に登録しました')
+      #フォームの用意
+      groupsform = GroupSelectForm(request.user, {'groups':sel_group})
+      friendsform = FriendsForm(request.user, friends=friends, vals=vlist)
+  #GET時の処理
+  else:
+    #フォームの用意
+    groupsform = GroupSelectForm(request.user)
+    friendsform = FriendsForm(request.user, friends=frieds, vals=[])
+    sel_group = '-'
+  
+  #共通の処理
+  createform = CreateGroupForm()
+  params = {
+    'login_user':request.user,
+    'groups_form':groupsform,
+    'create_form':createform,
+    'friend_form':friendsform,
+    'group':sel_group,
+  }
+  return render(request, 'sns/group.html', params)
+
+
